803f471a819a0ff14675b652cfadb05d1865372c
Lucas Lellis <lucaslellis777@gmail.com>
2019-09-01 16:04:12 -0300

    Hibernate setup

--- src/main/java/br/com/livro/domain/CarroDAO.java
+++ src/main/java/br/com/livro/domain/CarroDAO.java
@@ -1,233 +1,52 @@
 package br.com.livro.domain;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Query;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CarroDAO extends BaseDAO {
-	
-	public Carro getCarroById(Long id) throws SQLException {
-		Connection conn = null;
-		
-		PreparedStatement stmt = null;
-		
-		try {
+public class CarroDAO extends HibernateDAO<Carro> {
 
-			conn = getConnection();
-			stmt = conn.prepareStatement("select * from carro where id=?");
-			stmt.setLong(1, id);
-			
-			ResultSet rs = stmt.executeQuery();
-			if(rs.next()) {
-				Carro carro = createCarro(rs);
-				rs.close();
-				return carro;
-			}
-
-		} finally {
-			
-			if(stmt != null) {
-				stmt.close();
-			}
-			if(conn != null) {
-				conn.close();
-			}
-
-		}
-
-		return null;
+	public CarroDAO() {
+		super(Carro.class);
 	}
-	
-	public List<Carro> findByName(String name) throws SQLException {
-		Connection conn = null;
-		
-		PreparedStatement stmt = null;
-		List<Carro> carros = new ArrayList<Carro>();
 
-		try {
+	public Carro getCarroById(Long id) {
+		return super.get(id);
+	}
 
-			conn = getConnection();
-			stmt = conn.prepareStatement("select * from carro where lower(nome) like ?");
-			stmt.setString(1, name);
-			
-			ResultSet rs = stmt.executeQuery();
-			while(rs.next()) {
-				Carro carro = createCarro(rs);
-				carros.add(carro);
-			}
-			rs.close();
+	public List<Carro> findByName(String name) {
+		Query q = getSession().createQuery("from Carro where lower(nome) like lower(?)");
+		q.setString(0, "%" + name + "%");
 
-		} finally {
-			
-			if(stmt != null) {
-				stmt.close();
-			}
-			if(conn != null) {
-				conn.close();
-			}
-
-		}
-
+		List<Carro> carros = q.list();
 		return carros;
 	}
 
-	public List<Carro> findByTipo(String tipo) throws SQLException {
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		List<Carro> carros = new ArrayList<Carro>();
+	public List<Carro> findByTipo(String tipo) {
+		Query q = getSession().createQuery("from Carro where lower(tipo) like lower(?)");
+		q.setString(0, "%" + tipo + "%");
 
-		try {
-
-			conn = getConnection();
-			stmt = conn.prepareStatement("select * from carro where tipo = ?");
-			stmt.setString(1, tipo);
-			
-			ResultSet rs = stmt.executeQuery();
-			while(rs.next()) {
-				Carro carro = createCarro(rs);
-				carros.add(carro);
-			}
-			rs.close();
-
-		} finally {
-			
-			if(stmt != null) {
-				stmt.close();
-			}
-			if(conn != null) {
-				conn.close();
-			}
-
-		}
-
+		List<Carro> carros = q.list();
 		return carros;
 	}
-	
-	public List<Carro> getCarros() throws SQLException {
-		Connection conn = null;
-		
-		PreparedStatement stmt = null;
-		List<Carro> carros = new ArrayList<Carro>();
 
-		try {
+	public List<Carro> getCarros() {
+		Query q = getSession().createQuery("from Carro");
 
-			conn = getConnection();
-			stmt = conn.prepareStatement("select * from carro");
-			
-			ResultSet rs = stmt.executeQuery();
-			while(rs.next()) {
-				Carro carro = createCarro(rs);
-				carros.add(carro);
-			}
-			rs.close();
-
-		} finally {
-			
-			if(stmt != null) {
-				stmt.close();
-			}
-			if(conn != null) {
-				conn.close();
-			}
-
-		}
-
+		List<Carro> carros = q.list();
 		return carros;
 	}
-	
-	private Carro createCarro(ResultSet rs) throws SQLException {
-		Carro c = new Carro();
-		c.setId(rs.getLong("id"));
-		c.setNome(rs.getString("nome"));
-		c.setDesc(rs.getString("descricao"));
-		c.setUrlFoto(rs.getString("url_foto"));
-		c.setUrlVideo(rs.getString("url_video"));
-		c.setLatitude(rs.getString("latitude"));
-		c.setLongitude(rs.getString("longitude"));
-		c.setTipo(rs.getString("tipo"));
-		return c;
-	}
-	
-	public void save(Carro c) throws SQLException {
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		
-		try {
-			conn = getConnection();
-			if(c.getId() == null) {
-				stmt = conn.prepareStatement("insert into carro (nome, descricao, url_foto, url_video,latitude, longitude, tipo) "
-						+ " values (?,?,?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
-			} else {
-				stmt = conn.prepareStatement("update carro set nome=?, descricao=?, url_foto=?, url_video=?, latitude=?, longitude=?, tipo=? where id=?");
-			}
-			stmt.setString(1, c.getNome());
-			stmt.setString(2, c.getDesc());
-			stmt.setString(3, c.getUrlFoto());
-			stmt.setString(4, c.getUrlVideo());
-			stmt.setString(5, c.getLatitude());
-			stmt.setString(6, c.getLongitude());
-			stmt.setString(7, c.getTipo());
-			
-			if(c.getId() != null) {
-				stmt.setLong(8, c.getId());
-			}
-			
-			int count = stmt.executeUpdate();
-			if(count == 0) {
-				throw new SQLException("Erro ao inserir o carro");
-			}
-			
-			if(c.getId() == null) {
-				Long id = getGeneratedId(stmt);
-				c.setId(id);
-			}
 
-		} finally {
-			if(stmt != null) {
-				stmt.close();
-			}
-			if(conn != null) {
-				conn.close();
-			}
-		}
+	public void save(Carro c) {
+		super.save(c);
 	}
 
-	private Long getGeneratedId(PreparedStatement stmt) throws SQLException {
-		ResultSet rs = stmt.getGeneratedKeys();
-		if(rs.next()) {
-			Long id = rs.getLong(1);
-			return id;
-		}
-		return 0L;
-	}
-	
-	public boolean delete(Long id) throws SQLException {
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		
-		try {
-			conn = getConnection();
-			stmt = conn.prepareStatement("delete from carro where id=?");
-			stmt.setLong(1, id);
-			
-			int count = stmt.executeUpdate();
-			boolean ok = count > 0;
-			return ok;
-		} finally {
-			if(stmt != null) {
-				stmt.close();
-			}
-			if(conn != null) {
-				conn.close();
-			}
-		}
+	public boolean delete(Long id) {
+		Carro c = get(id);
+		delete(c);
+
+		return true;
 	}
 }
- 
